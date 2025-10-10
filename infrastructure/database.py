b06375a42493fc2393517c0ecb45c8b19cad309a
Karimov Dmitry <dimakarimov55555@gmail.com>
2025-10-10 21:35:05 +0700

    renamed methods

--- infrastructure/database.py
+++ infrastructure/database.py
@@ -13,20 +13,22 @@ class AppDbContext:
         if self.pool:
             return await self.pool.close
 
+    # INSERT/UPDATE/DELETE (вернёт статус)
     async def execute(self, query : str, *args) -> str:
         async with self.pool.acquire() as conn:
             return await conn.execute(query, *args)
 
-    async def fetch(self, query : str, *args) -> List[asyncpg.Record]:
+    # Выборка
+    async def fetch_all(self, query : str, *args) -> List[asyncpg.Record]:
         async with self.pool.fetch() as conn:
             return await conn.fetch(query, *args)
     
-    async def fetchrow(self, query : str, *args) -> Optional[asyncpg.Record]:
+    async def fetch_one(self, query : str, *args) -> Optional[asyncpg.Record]:
         async with self.pool.acquire() as conn:
             return await conn.fetchrow(query, *args)
     
-    async def fetchval(self, query : str, *args):
+    # INSERT с возвратом ID
+    async def execute_returning_id(self, query : str, *args):
         async with self.pool.acquire() as conn:
             return await conn.fetchval(query, *args)
-    
     
