378cd62a7a287b3d219ffb2390760803ba0a26bb
Karimov Dmitry <dimakarimov55555@gmail.com>
2025-10-12 14:23:32 +0700

    добавил асинхронность для работы БД

--- core/config.py
+++ core/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings): # класс настроек будет меняться 
     DATABASE_URL: str =\
-    "postgresql+psycopg2://postgres:123456789@localhost:5432/cat_haircut" # 
+    "postgresql+asyncpg://postgres:123456789@localhost:5432/cat_haircut" # 
     NEUTRAL_API_URL: str = "http://localhost:5050"
     UPLOAD_DIR: str = "uploads"
 
