71f83baa2022fdfd37203f9d9e296a21d4fff59f
Karimov Dmitry <dimakarimov55555@gmail.com>
2025-10-12 20:32:17 +0700

    создал датаклассы для общения с API

---
+++ api/schemas.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from pydantic import BaseModel
+from typing import List
+
+@dataclass
+class SessionCreateResponse(BaseModel):
+    session_id: str
+    
+@dataclass
+class ImageUploadRequest(BaseModel):
+    session_id: str
+    cat_id : int
+    upload_timestamp : float # секунды
+    image_count : int
+
+@dataclass
+class ProcessImagesResponse(BaseModel):
+    cat_id: int
+    status: str
+
+@dataclass
+class RecommendationResponse(BaseModel):
+    haircuts: List[str]
+    reasoning: str
