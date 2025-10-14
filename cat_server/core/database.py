2e6a3485d5ed279e99fe65e38afdd5d214dc16c6
Karimov Dmitry <dimakarimov55555@gmail.com>
2025-10-14 20:28:34 +0700

    добавил метод проверки подключения к БД по средствам реализации запроса

--- cat_server/core/database.py
+++ cat_server/core/database.py
@@ -1,3 +1,4 @@
+from sqlalchemy import text
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from config import settings
 
@@ -12,3 +13,14 @@ AsyncSessionLocal = async_sessionmaker(
     class_=AsyncSession,
     expire_on_commit=False
 )
+
+async def check_database_connection():
+    async with AsyncSessionLocal() as session:
+        try:
+            # Простой запрос для проверки соединения
+            await session.execute(text("SELECT 1"))
+            return True
+        except Exception as e:
+            print(f"Database connection error: {e}")
+            raise
+
