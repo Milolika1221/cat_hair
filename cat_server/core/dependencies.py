d79bbe591b3ea593c9f3effc2896bb4ceac9ac15
Karimov Dmitry <dimakarimov55555@gmail.com>
2025-10-16 21:55:05 +0700

    исправил ошибки сессии

--- cat_server/core/dependencies.py
+++ cat_server/core/dependencies.py
@@ -1,10 +1,11 @@
 from fastapi import Depends
-from infrastructure.repositories import CatsRepository, CatImagesRepository, CatCharacteristicsRepository, HaircutsRepository,RecommendationRepository
-from services.user_session_service import UserSessionService
-from services.image_processing_service import ImageProcessingService
-from services.recommendation_service import RecommendationService
-from core.database import AsyncSessionLocal
-from core.config import settings
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from cat_server.infrastructure.repositories import CatsRepository, CatImagesRepository, CatCharacteristicsRepository, HaircutsRepository,RecommendationRepository
+from cat_server.services.image_processing_service import ImageProcessingService, NeuralNetworkClient, UserSessionService
+from cat_server.services.recommendation_service import RecommendationService
+from cat_server.core.database import AsyncSessionLocal
+from cat_server.core.config import settings
 
 
 async def get_db_session():
@@ -12,28 +13,27 @@ async def get_db_session():
         try:
             yield session
         finally:
-            session.close
+            await session.close()
 
 def get_user_session_service() -> UserSessionService :
     return UserSessionService()
 
 def get_image_processing_service(
         user_session : UserSessionService = Depends(get_user_session_service),
-        db_session = Depends(get_db_session)
+        db_session : AsyncSession= Depends(get_db_session)
     ) -> ImageProcessingService:
-    from infrastructure.neural_client import NeuralNetworkClient
     cats_repo = CatsRepository(db_session)
     images_repo = CatImagesRepository(db_session)
     characteristic_repo = CatCharacteristicsRepository(db_session)
-    
-    neutral_client = NeuralNetworkClient(base_url=settings.NEUTRAL_API_URL, timeout=settings.NEURAL_API_TIMEOUT)
+
+    neural_client = NeuralNetworkClient(base_url=settings.NEUTRAL_API_URL, timeout=settings.NEURAL_API_TIMEOUT)
 
     return ImageProcessingService(
         user_session_service=user_session,
-        cats_repository=cats_repo,
-        images_repository=images_repo,
-        characteristics_repository=characteristic_repo,
-        neutral_client=neutral_client
+        cats_repo=cats_repo,
+        images_repo=images_repo,
+        characteristics_repo=characteristic_repo,
+        neural_client=neural_client
         )
 
 def get_recommendation_service(
@@ -42,7 +42,7 @@ def get_recommendation_service(
     characteristics_repo = CatCharacteristicsRepository(db_session)
     haircuts_repo = HaircutsRepository(db_session)
     recommendations_repo = RecommendationRepository(db_session)
-    
+
     return RecommendationService(
         characteristics_repository=characteristics_repo,
         haircuts_repository=haircuts_repo,
