67b104933e40b85a6f5f16e14c7e4696bb298ba8
Karimov Dmitry <dimakarimov55555@gmail.com>
2025-10-16 22:13:58 +0700

    сервис изображений стал более точным

--- cat_server/domain/interfaces.py
+++ cat_server/domain/interfaces.py
@@ -1,15 +1,15 @@
 from typing import List
 from abc import ABC, abstractmethod
 
-from domain.dtos import ImageData, ProcessingResult, RecommendationResult, SessionData, ValidationResult
+from cat_server.domain.dto import ImageData, ProcessingResult, RecommendationResult, SessionData, ValidationResult
 
 
 class IUserSessionService(ABC):
     @abstractmethod
     async def create_session(self) -> str:  # возвращает session_id
         pass
-    
-    @abstractmethod 
+
+    @abstractmethod
     async def get_session(self, session_id: str) -> SessionData:
         pass
 
@@ -23,7 +23,7 @@ class IUserSessionService(ABC):
 
 class IImageProcessingService(ABC):
     @abstractmethod
-    async def process_images(self, session_id: str) -> ProcessingResult:
+    async def process_images(self, session_id: str, cat_id : int, images_data : List[ImageData]) -> ProcessingResult:
         pass
 
     @abstractmethod
