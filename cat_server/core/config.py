5202cba1f0c06c296cd3c74a21367bddd54115df
Karimov Dmitry <dimakarimov55555@gmail.com>
2025-10-16 20:16:18 +0700

    изменил адрес БД и наименование сервера

--- cat_server/core/config.py
+++ cat_server/core/config.py
@@ -2,14 +2,14 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings): 
     DATABASE_URL: str =\
-    "postgresql+asyncpg://postgres:123456789@localhost:5432/cat_haircut" # 
+    "postgresql+asyncpg://postgres:123456789@localhost:5432/cat-haircut"
 
     NEUTRAL_API_URL: str = "https://localhost:8050"
     UPLOAD_DIR: str = "uploads"
     NEURAL_API_TIMEOUT: int = 60
     
     MAX_FILE_SIZE: int = 10 * 1024 * 1024  # 10MB
-    APP_TITLE: str = "Cat Grooming API"
+    APP_TITLE: str = "Cat AI API"
     APP_VERSION: str = "1.0.0"
     
     class Config:
