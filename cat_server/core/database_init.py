13a0d0554d0b949b62f376ce25ec9e494304602f
Karimov Dmitry <dimakarimov55555@gmail.com>
2025-11-18 19:56:10 +0700

    style: изменён стиль кода, теперь более грамотный

--- cat_server/core/database_init.py
+++ cat_server/core/database_init.py
@@ -1,20 +1,22 @@
 import asyncio
 
-from cat_server.domain.entities import  Base
-from cat_server.core.database import engine, test_engine
+from cat_server.core.database import engine
+from cat_server.domain.entities import Base
+
 
 async def create_database():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     print("Все таблицы созданы!")
 
+
 async def drop_database():
-    async with engine.begin() as conn :
+    async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
-    print ('deleted')
+    print("deleted")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(create_database())
 
 # if __name__ == '__main__':
