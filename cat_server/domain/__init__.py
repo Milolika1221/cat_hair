0fa8fc1dc71c3596d6449b4e37f6ca16eaecb9f1
Karimov Dmitry <dimakarimov55555@gmail.com>
2025-11-18 19:56:40 +0700

    style: изменён заголовочный файл

--- cat_server/domain/__init__.py
+++ cat_server/domain/__init__.py
@@ -1,19 +1,43 @@
-from .entities import Cats, CatImages, CatCharacteristics, Haircuts,Recommendations
-from .interfaces import IUserSessionService,IImageProcessingService,IRecommendationService
-from  .dto import *
+from .dto import (
+    AnalysisResult,
+    HaircutRecommendation,
+    ImageData,
+    ImageProcessingResponse,
+    NeuralNetworkRequest,
+    NeuralNetworkResponse,
+    ProcessingException,
+    ProcessingResult,
+    RecommendationResult,
+    ScoredHaircut,
+    SessionData,
+    ValidationResult,
+)
+from .entities import CatCharacteristics, CatImages, Cats, Haircuts, Recommendations
+from .interfaces import (
+    IImageProcessingService,
+    IRecommendationService,
+    IUserSessionService,
+)
+
 __all__ = [
-    'Cats',
-    'CatImages',
-    'CatCharacteristics',
-    'Haircuts',
-    'Recommendations',
-    'IUserSessionService',
-    'IImageProcessingService',
-    'IRecommendationService',
-    'ImageData', 'SessionData',
-    'AnalysisResult', 'ProcessingResult',
-    'ProcessingResult', 'ImageProcessingResponse',
-    'ValidationResult', 'NeuralNetworkResponse',
-    'NeuralNetworkRequest', 'HaircutRecommendation',
-    'ScoredHaircut', 'RecommendationResult', 'ProcessingException'
+    "Cats",
+    "CatImages",
+    "CatCharacteristics",
+    "Haircuts",
+    "Recommendations",
+    "IUserSessionService",
+    "IImageProcessingService",
+    "IRecommendationService",
+    "ImageData",
+    "SessionData",
+    "AnalysisResult",
+    "ProcessingResult",
+    "ImageProcessingResponse",
+    "ValidationResult",
+    "NeuralNetworkResponse",
+    "NeuralNetworkRequest",
+    "HaircutRecommendation",
+    "ScoredHaircut",
+    "RecommendationResult",
+    "ProcessingException",
 ]
