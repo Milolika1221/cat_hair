fc5a6e1410f5e7ce53a59396c0255222ba57336e
Karimov Dmitry <dimakarimov55555@gmail.com>
2025-11-19 18:40:54 +0700

    Update __init__.py

--- cat_server/api/__init__.py
+++ cat_server/api/__init__.py
@@ -1,19 +1,39 @@
 # from .endpoints import router
-from .schemas import (
-    ImageData, SessionCreateResponse, ImageUploadResponse,
-    SessionData, ValidationResult, AnalysisResult,
-    HaircutRecommendation, RecommendationResult,
-    ScoredHaircut, ProcessingError, ProcessingResult, ImageProcessingResponse,
-    NeuralNetworkResponse, NeuralNetworkRequest
+from ..domain.dto import (
+    AnalysisWithRecommendations,
+    ImageData,
+    NeuralNetworkRequest,
+    NeuralNetworkResponse,
+    ProcessingError,
+    ProcessingResult,
+    RecommendationResult,
+    ScoredHaircut,
+    SessionData,
+    ValidationResult,
 )
-
+from .schemas import (
+    AnalysisResult,
+    HaircutRecommendation,
+    ImageProcessingResponse,
+    ImageUploadResponse,
+    SessionCreateResponse,
+)
 
 __all__ = [
     # "router",
-    "SessionCreateResponse", "ImageUploadResponse",
-    "SessionData", "ValidationResult", "AnalysisResult",
-    "HaircutRecommendation", "RecommendationResult",
-    "ScoredHaircut", "ProcessingError", "ProcessingResult",
-    "ImageProcessingResponse", "NeuralNetworkResponse", "NeuralNetworkRequest",
-    "ImageData"
+    "SessionCreateResponse",
+    "ImageUploadResponse",
+    "SessionData",
+    "ValidationResult",
+    "AnalysisResult",
+    "HaircutRecommendation",
+    "RecommendationResult",
+    "ScoredHaircut",
+    "ProcessingError",
+    "ProcessingResult",
+    "ImageProcessingResponse",
+    "NeuralNetworkResponse",
+    "NeuralNetworkRequest",
+    "ImageData",
+    "AnalysisWithRecommendations",
 ]
