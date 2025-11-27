d776764dcc708c05889f1cf74e5c7f5e30fddbbe
Karimov Dmitry <dimakarimov55555@gmail.com>
2025-11-27 21:22:40 +0700

    feat: энпоинты принимают и отправляют по одному изображению

--- cat_server/api/endpoints.py
+++ cat_server/api/endpoints.py
@@ -1,7 +1,8 @@
+import base64
 from datetime import datetime
-from typing import Annotated, List
+from typing import Annotated
 
-from fastapi import APIRouter, Depends, File, HTTPException, Path, UploadFile
+from fastapi import APIRouter, Depends, File, HTTPException, Path, UploadFile, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from cat_server.api.schemas import (
@@ -12,19 +13,17 @@ from cat_server.api.schemas import (
 from cat_server.core.dependencies import (
     get_db_session,
     get_image_processing_service,
-    get_recommendation_service,
     get_user_session_service,
 )
 from cat_server.domain.dto import ImageData, ProcessingException
 from cat_server.infrastructure.repositories import CatsRepository
 from cat_server.services.image_processing_service import ImageProcessingService
-from cat_server.services.recommendation_service import RecommendationService
 from cat_server.services.user_session_service import UserSessionService
 
 router = APIRouter()
 
 
-@router.post("/sessions", response_model=SessionCreateResponse)
+@router.post("/session", response_model=SessionCreateResponse)
 async def create_session(
     user_session_service: UserSessionService = Depends(get_user_session_service),
 ):
@@ -32,40 +31,38 @@ async def create_session(
     return SessionCreateResponse(session_id=session_id)
 
 
-@router.post("/{session_id}/{cat_id}/images", response_model=List[ImageUploadResponse])
+@router.post("/{session_id}/{cat_id}/images", response_model=ImageUploadResponse)
 async def upload_images(
     session_id: str,  # session_id передаётся в URL
     cat_id: Annotated[
         int, Path(..., ge=0)
     ],  # Основной тип параметра int; Path - доп метаданные (валидация), ge (greater or equal) не даст ввести неправильный id
-    files: List[UploadFile] = File(..., max_files=5),
+    file: UploadFile = File(...),
     user_session_service: UserSessionService = Depends(get_user_session_service),
     image_processing_service: ImageProcessingService = Depends(
         get_image_processing_service
     ),
     db_session: AsyncSession = Depends(get_db_session),
 ):
+    """Функция для загрузки изображений кота в сервис нейросети и получения результатов"""
     start_time = datetime.now()
-    images_data = []
-    for file in files:
-        image_bytes = await file.read()
 
-        content_type = file.content_type or "unknown"
-        format = (
-            content_type.split("/")[-1].upper() if "/" in content_type else "UNKNOWN"
-        )
+    # Заполнение данных изображений
+    image_bytes = await file.read()
+    content_type = file.content_type or "unknown"
+    format = content_type.split("/")[-1].upper() if "/" in content_type else "unknown"
+    image_data = ImageData(
+        file_name=file.filename or "unnamed.jpg",  # pyright: ignore[reportArgumentType]
+        data=image_bytes,
+        size=len(image_bytes),
+        format=format,
+        uploaded_at=datetime.now(),
+    )
+    await file.close()
 
-        image_data = ImageData(
-            file_name=file.filename,  # pyright: ignore[reportArgumentType]
-            data=image_bytes,
-            size=len(image_bytes),
-            format=format,
-            uploaded_at=datetime.now(),
-        )
-        images_data.append(image_data)
-
-    success = await user_session_service.add_images_to_session(
-        session_id=session_id, images_data=images_data
+    # Добавляем изображения в сессию
+    success = await user_session_service.add_image_to_session(
+        session_id=session_id, image_data=image_data
     )
     if not success:
         raise HTTPException(status_code=404, detail="Failed to store images in session")
@@ -83,25 +80,23 @@ async def upload_images(
 
     try:
         result = await image_processing_service.process_images(
-            session_id=session_id, cat_id=cat_id, images_data=images_data
+            session_id=session_id, cat_id=cat_id, image_data=image_data
         )
         if result.status == "error":
             raise HTTPException(
                 status_code=400,
                 detail=result.error.message if result.error else "Processing Failed",
             )
-        responses = []
+
+        # Формируем ответы пользователю
         curr_time = datetime.now().timestamp() - start_time.timestamp()
-        for i, img in enumerate(images_data):
-            responses.append(
-                ImageUploadResponse(
-                    cat_id=cat_id,
-                    session_id=session_id,
-                    file_name=img.file_name,
-                    upload_timestamp=curr_time,
-                )
-            )
-        return responses
+        response = ImageUploadResponse(
+            cat_id=cat_id,
+            session_id=session_id,
+            file_name=image_data.file_name,
+            upload_timestamp=curr_time,
+        )
+        return response
 
     except ProcessingException as e:
         raise HTTPException(
@@ -121,26 +116,25 @@ async def upload_images(
 async def get_cat_recommendations(
     session_id: str,
     cat_id: int,
-    recommendation_service: RecommendationService = Depends(get_recommendation_service),
     image_processing_service: ImageProcessingService = Depends(
         get_image_processing_service
     ),
 ):
-    proc_result = await image_processing_service.get_processing_result(
-        session_id=session_id, cat_id=cat_id
-    )
-    if not proc_result:
-        raise HTTPException(
-            status_code=404,
-            detail="No processing result found for this cat. Upload images first.",
-        )
+    """Формирование полноценного ответа пользователю"""
 
     # Получаем рекомендации
-    rec_result = await recommendation_service.get_recommendations(cat_id=cat_id)
+    rec_result = await image_processing_service.get_processing_result(
+        cat_id,
+    )
+
+    if rec_result is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Recommendations for cat_id={cat_id} not found",
+        )
 
     return CatRecommendationsResponse(
-        cat_id=cat_id,
-        recommendations=rec_result.recommendations,
-        characteristics=proc_result.characteristics,
-        processed_images=proc_result.processed_images,
+        cat_id=rec_result["cat_id"],
+        image=base64.b64encode(rec_result["image"]).decode("utf-8"),
+        recommendation=rec_result["recommendation"],
     )
